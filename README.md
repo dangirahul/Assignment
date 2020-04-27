f73e49544b3ddec8d952181924e6ecae61bc0148
dangirahul <buntid54@gmail.com>
2020-04-27 23:47:02 +0530

    Changes made in guessinggame.sh

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Guessing Game Project
 Makefile Run at :
-04-27-2020 00:40:08
+04-27-2020 23:44:48
 
 guessinggame.sh contains the following number of line of code:
 34
--- guessinggame.sh
+++ guessinggame.sh
@@ -2,33 +2,32 @@
 # File: guessinggame.sh
 status=0
 
-isfile() {
-local count_file=0
-if [[ -f "$1" ]]
-then 
-let count_file=$count_file+1
-fi
-echo $count_file
+filecount() {
+	local count_file=0
+	let count_file=$(ls -la | grep "^[-]" | wc -l)
+	echo $count_file
 }
 while [ $status -eq 0 ]
 do
-echo "Guess How many files are present in current directory ?"
-read response
-check_file_count=0
-file_list=$(ls)
-for f in $file_list 
-do	
-	let check_file_count=$check_file_count+$(isfile $f)
-done
-if [[ $check_file_count -eq $response ]]
-then
-echo "Congratulations! you guess is correct. Number of files in current directory is $response"
-let status=1
-elif [[ $check_file_count -lt $response ]]
-then
-echo "Your guess is too high please try again !"
-else
-echo "your guess is too low please try again !"
-fi
+	echo "Guess How many files are present in current directory ?"
+	read response
+	correct_count=`filecount`
+
+	if [[ $response =~ ^[0-9] ]]
+	then
+		
+		if [[ $correct_count -eq $response ]]
+		then
+			echo "Congratulations! you guess is correct. Number of files in current directory is $response"
+			let status=1
+		elif [[ $correct_count -lt $response ]]
+		then
+			echo "Your guess is too high please try again !"
+		else
+			echo "your guess is too low please try again !"
+		fi
+	else
+	echo " You have entered an invalid number ! please enter a valid number"
+        fi
 done
 
